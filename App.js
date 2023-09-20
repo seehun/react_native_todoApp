52f70b38e2f060deef95cc3ba7990cf7c96095a9
millifail <hwangbird99@gmail.com>
2023-09-21 00:10:36 +0900

    react navigation 설정

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import { StyleSheet, Text, View } from 'react-native';
 import MainScreen from './screens/MainScreen';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Main' component={MainScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
