692260e391f2731923b01041a87c6f8ad030d570
millifail <hwangbird99@gmail.com>
2023-09-20 10:17:59 +0900

    redux slice 및 store 생성

---
+++ redux/slices/TodoSlice.js
@@ -0,0 +1,33 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const todoSlice = createSlice({
+  name: 'todo',
+  initialState: {
+    currentId: 4,
+    todos: [],
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({
+        id: state.currentId++,
+        text: action.payload.trim(),
+        state: 'todo',
+      });
+    },
+    updateTodo: (state, action) => {
+      const item = state.todos.findIndex((item) => item.id === action.payload);
+      state.todos[item].state =
+        state.todos[item].state === 'todo' ? 'done' : 'todo';
+      state.todos.push(state.todos.splice(item, 1)[0]);
+    },
+    delteTodo: (state, action) => {
+      const item = state.todos.findIndex((item) => item.id === action.payload);
+      if (item > -1) {
+        state.todos.splice(item, 1);
+      }
+    },
+  },
+});
+
+export default todoSlice.reducer;
+export const { addTodo, updateTodo, delteTodo } = todoSlice.actions;
